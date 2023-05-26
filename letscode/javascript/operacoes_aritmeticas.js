58d8f0a94b34b9797f5bbfdf6ccbdf8744d602de
314u <pyuloko@outlook.com>
2023-05-25 23:47:23 -0300

    for (let i = 0; i <= 9; i+=2) {      console.log(i); }

---
+++ letscode/javascript/operacoes_aritmeticas.js
@@ -0,0 +1,11 @@
+let numero1 = 10
+let numero2 = 3
+
+console.log('Soma dos numeros : ', numero1 + numero2);
+console.log('Subtração dos numeros : ', numero1 - numero2);
+console.log('Multiplicação dos numeros : ', numero1 * numero2);
+console.log('Divisão dos numeros : ', numero1 / numero2);
+console.log('Potencia dos numeros : ', numero1 ** numero2);
+console.log('Resto da divisão dos numeros : ', numero1 % numero2);
+
+console.log('Pi : ', Math.PI);
